c4032afba4886fd57a3f5b4c948601d76d59705a
Théophile <theophile@sogilis.com>
2022-01-10 18:43:57 +0100

    feat: initial commit of webapp

---
+++ voogle-webapp/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Gallery/>
+  </div>
+</template>
+
+<script>
+import Gallery from '@/components/Gallery.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Gallery
+  }
+}
+</script>
