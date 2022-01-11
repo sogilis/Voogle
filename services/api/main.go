06ea3f95b35b1e42190cdba8e76e9bcf1e1f3e1d
Jordan Prudhomme <jordan@sogilis.com>
2022-01-11 11:12:48 +0100

    feat: start http server

--- services/api/main.go
+++ services/api/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Main")
+	log.Info("Starting Voogle API")
 
+	r := mux.NewRouter()
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf("0.0.0.0:%v", 4444),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal("Crashed with error: ", err)
+	}
 }
