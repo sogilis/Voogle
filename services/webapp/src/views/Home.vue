15c5b3d8c8e03f9ccd16b5af215b43b02525783f
Théophile <theophile@sogilis.com>
2022-01-12 10:09:55 +0100

    feat(webapp): Home add logo voogle

--- services/webapp/src/views/Home.vue
+++ services/webapp/src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Gallery/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Sqsc" src="../assets/sqsc.jpeg" />
+    <img alt="Sqsc" src="../assets/sqsc.jpeg" />
+    <img alt="g" src="../assets/g.png" />
+    <img alt="l" src="../assets/l.png" />
+    <img alt="e" src="../assets/e.png" />
+
+    <Gallery />
   </div>
 </template>
 
 <script>
-import Gallery from '@/components/Gallery.vue'
+import Gallery from "@/components/Gallery.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Gallery
-  }
-}
+    Gallery,
+  },
+};
 </script>
+
+<style scoped lang="scss">
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
