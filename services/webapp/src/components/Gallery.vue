6375afed1f6117ede72e712c057d0144df934ba3
Théophile <theophile@sogilis.com>
2022-01-14 09:53:04 +0100

    feat(webapp): add basic auth and clean code

--- services/webapp/src/components/Gallery.vue
+++ services/webapp/src/components/Gallery.vue
@@ -4,13 +4,12 @@
     <div class="wrapper">
       <Video v-for="video in videos" :key="video" v-bind:video="video" />
     </div>
-    <div class="button-generator">
-      <button v-on:click="generateVideo">Generate video</button>
-    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 import Video from "@/components/Video.vue";
 
 export default {
@@ -20,10 +19,24 @@ export default {
       videos: [],
     };
   },
-  methods: {
-    generateVideo: function () {
-      this.videos = { title: "Video " };
-    },
+  mounted() {
+    axios
+      .get("http://localhost:4444/api/v1/videos", {
+        auth: {
+          username: "dev",
+          password: "test",
+        },
+      })
+      .then((response) => {
+        this.videos = response.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   },
   components: {
     Video,
@@ -37,9 +50,5 @@ export default {
   grid-template-columns: repeat(1, 1fr);
   grid-gap: 10px;
   grid-auto-rows: minmax(100px, auto);
-}
-
-.button-generator {
-  margin: 1%;
 }
 </style>
