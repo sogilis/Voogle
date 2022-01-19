d04e714ad6764a91c4157016c7f0c42ce26b4811
Jordan Prudhomme <jordan@sogilis.com>
2022-01-19 20:32:06 +0100

    feat(api): add env var for dev mode

--- services/api/config/config.go
+++ services/api/config/config.go
@@ -8,6 +8,7 @@ type Config struct {
 	Port     uint32 `env:"PORT" envDefault:"4444"`
 	UserAuth string `env:"USER_AUTH,required"`
 	PwdAuth  string `env:"PWD_AUTH,required"`
+	IsDev    bool   `env:"DEV_MODE" envDefault:"false"`
 }
 
 func NewConfig() (*Config, error) {
