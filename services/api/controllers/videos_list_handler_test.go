57a7bc80f5fc20d93036b47f6fd8e1f0fe53687e
Jordan Prudhomme <jordan@sogilis.com>
2022-01-19 11:11:55 +0100

    refacto(api): use New router in tests with auth

--- services/api/controllers/videos_list_handler_test.go
+++ services/api/controllers/videos_list_handler_test.go
@@ -7,10 +7,11 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/Sogilis/Voogle/services/api/config"
 	. "github.com/Sogilis/Voogle/services/api/controllers"
+	. "github.com/Sogilis/Voogle/services/api/router"
 )
 
 func TestVideosListHandler(t *testing.T) {
@@ -23,11 +24,17 @@ func TestVideosListHandler(t *testing.T) {
 	allVideosExpected := AllVideos{Status: "Success", Data: []VideoInfo{{Id: "video1", Title: "video1"}, {Id: "video2", Title: "video2"}}}
 	w := httptest.NewRecorder()
 
+	testUsername := "dev"
+	testUsePwd := "test"
+
 	// When
-	r := mux.NewRouter()
-	r.PathPrefix("/api/v1/videos/list").Handler(VideosListHandler{}).Methods("GET")
+	r := NewRouter(config.Config{
+		UserAuth: testUsername,
+		PwdAuth:  testUsePwd,
+	})
 
 	req := httptest.NewRequest("GET", "/api/v1/videos/list", nil)
+	req.SetBasicAuth(testUsername, testUsePwd)
 	r.ServeHTTP(w, req)
 
 	// Then
