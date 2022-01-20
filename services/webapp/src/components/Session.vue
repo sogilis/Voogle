a5327ad816e518991cc0c12a552310dd96e978be
Théophile <theophile@sogilis.com>
2022-01-20 12:02:28 +0100

    feat(webapp): display or not login and logout

--- services/webapp/src/components/Session.vue
+++ services/webapp/src/components/Session.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="login">
-    <h2>Login</h2>
-    <form @submit.prevent="login">
-      <p>
-        <label for="username">Username</label>
-        <input id="username" v-model="username" name="username"><br>
-        <label for="password">Password</label>
-        <input id="password" v-model="password" name="password">
-      </p>
-      <input type="submit" value="login" v-on:click="login">
-    </form>
-  </div>
-  <div class="logout">
-    <button v-on:click="logout">Logout</button>
+  <div>
+    <div v-if="cookies == undefined" class="login">
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <p>
+          <label for="username">Username</label>
+          <input id="username" v-model="username" name="username" /><br />
+          <label for="password">Password</label>
+          <input id="password" v-model="password" name="password" />
+        </p>
+        <input type="submit" value="login" v-on:click="login" />
+      </form>
+    </div>
+    <div v-else class="logout">
+      <button v-on:click="logout">Logout</button>
+    </div>
   </div>
 </template>
 
@@ -25,21 +27,28 @@ export default {
     return {
       username: null,
       password: null,
-    }
+      cookies: undefined,
+    };
   },
-  methods:
-    {
-      login: function () {
-        cookies.get()
-        cookies.set("Authorization", "Basic " + btoa(this.username + ":" + this.password), {expires: 30})
-      },
-      logout: function (){
-        cookies.remove("Authorization")
-      }
-    }
-}
+  mounted() {
+    this.cookies = this.getCookies();
+  },
+  methods: {
+    login: function () {
+      cookies.set(
+        "Authorization",
+        "Basic " + btoa(this.username + ":" + this.password),
+        { expires: 30 }
+      );
+      this.cookies = this.getCookies();
+    },
+    logout: function () {
+      cookies.remove("Authorization");
+      this.cookies = this.getCookies();
+    },
+    getCookies: function () {
+      return cookies.get("Authorization");
+    },
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
