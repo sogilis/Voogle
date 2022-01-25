0c1e64f456cc5a0949b80f7a4a1614536e0b86f4
Jordan Prudhomme <jordan@sogilis.com>
2022-01-25 14:25:46 +0100

    feat(webapp): add dedicated page to view specific video

--- services/webapp/src/router/index.js
+++ services/webapp/src/router/index.js
@@ -8,13 +8,10 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/watch/:id",
+    name: "Watch",
     component: function () {
-      return import(/* webpackChunkName: "about" */ "../views/About.vue");
+      return import("../views/Watch.vue");
     },
   },
 ];
