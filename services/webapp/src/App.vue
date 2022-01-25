951c344103c2ec450eb6aca705853569a8b86b52
Jordan Prudhomme <jordan@sogilis.com>
2022-01-25 14:24:22 +0100

    feat(webapp): remove about page

--- services/webapp/src/App.vue
+++ services/webapp/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav" class="topnav">
     <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
     <Session></Session>
   </div>
   <router-view />
---
+++ services/webapp/src/views/Watch.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Watch.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
