9b4eea6f1364e139529449efa390d72be32670e0
Jordan Prudhomme <jordan@sogilis.com>
2022-01-25 14:25:46 +0100

    feat(webapp): add dedicated page to view specific video

--- services/webapp/src/views/Watch.vue
+++ services/webapp/src/views/Watch.vue
@@ -1,10 +1,16 @@
 <template>
-$END$
+  <h1>WATCHING</h1>
+  <Video :key="{id: this.$route.params.id, title: this.$route.params.id}" v-bind:video="{id: this.$route.params.id, title: this.$route.params.id}" />
 </template>
 
 <script>
+import Video from '@/components/Video.vue';
+
 export default {
-name: "Watch.vue"
+  name: "Watch.vue",
+  components: {
+    Video,
+  },
 }
 </script>
 
