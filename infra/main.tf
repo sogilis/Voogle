81d002d3e93bb8e864f393167c1c27fb869f1d51
Théophile <theophile@sogilis.com>
2022-01-26 08:34:45 +0100

    feat(infra): add infra code terraform

---
+++ infra/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-sogilis-production"
+    dynamodb_table = "tfstate-lock-dynamo"
+    key            = "voogle/terraform.tfstate"
+    region         = "eu-west-3"
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-3"
+}
+
+resource "aws_organizations_organization" "sogilis" {
+  # (resource arguments)
+}
+
+module "voogle-sogilis-dev" {
+  root_organization = aws_organizations_organization.sogilis.roots[0].id
+  source            = "./voogle-sogilis-dev"
+}
+
+output "account-id" {
+  value = module.voogle-sogilis-dev.account-id
+}
+
+output "token-id" {
+  value = module.voogle-sogilis-dev.token-id
+}
+
+output "secret" {
+  value     = module.voogle-sogilis-dev.secret
+  sensitive = true
+}
+
+output "voogle-s3-token-id" {
+  value = module.voogle-sogilis-dev.voogle-s3-token-id
+}
+
+output "voogle-s3-secret" {
+  value     = module.voogle-sogilis-dev.voogle-s3-secret
+  sensitive = true
+}
---
+++ infra/voogle-sogilis-dev/group.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_group" "voogle-s3-group" {
+  name = "voogle-s3-group"
+  path = "/voogle-sogilis-dev/"
+}
+
+resource "aws_iam_group_policy" "voogle-s3-group-policy" {
+  name  = "voogle-s3-group-policy"
+  group = aws_iam_group.voogle-s3-group.name
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket",
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.voogle-video-s3-bucket.bucket}",
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.voogle-video-s3-bucket.bucket}/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Decrypt",
+          "kms:GenerateDataKey",
+        ],
+        "Resource" : [
+          "arn:aws:kms:eu-west-3:341514320952:key/f889ca9c-80cc-4cf5-b593-736071618385"
+        ]
+      },
+    ]
+  })
+}
