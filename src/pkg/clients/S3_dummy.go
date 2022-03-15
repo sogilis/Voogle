6ee3af0f2e5a2e3b240e115db29d68eb4e8dd329
RaboliotLeGris <jordan@raboland.fr>
2022-03-15 18:44:18 +0100

    feat: create bucket at the creation of the client if it doesn't exist

--- src/pkg/clients/S3_dummy.go
+++ src/pkg/clients/S3_dummy.go
@@ -11,10 +11,11 @@ type s3ClientDummy struct {
 	listObjects    func() ([]string, error)
 	getObject      func(id string) (io.Reader, error)
 	putObjectInput func(f io.Reader, title string) error
+	createBucket   func(n string) error
 }
 
-func NewS3ClientDummy(listObjects func() ([]string, error), getObject func(string) (io.Reader, error), putObjectInput func(io.Reader, string) error) IS3Client {
-	return s3ClientDummy{listObjects, getObject, putObjectInput}
+func NewS3ClientDummy(listObjects func() ([]string, error), getObject func(string) (io.Reader, error), putObjectInput func(io.Reader, string) error, createBucket func(n string) error) IS3Client {
+	return s3ClientDummy{listObjects, getObject, putObjectInput, createBucket}
 }
 
 func (s s3ClientDummy) ListObjects(ctx context.Context) ([]string, error) {
@@ -27,4 +28,8 @@ func (s s3ClientDummy) GetObject(ctx context.Context, id string) (io.Reader, err
 
 func (s s3ClientDummy) PutObjectInput(ctx context.Context, f io.Reader, title string) error {
 	return s.putObjectInput(f, title)
+}
+
+func (s s3ClientDummy) CreateBucketIfDoesNotExists(ctx context.Context, bucketName string) error {
+	return s.createBucket(bucketName)
 }
