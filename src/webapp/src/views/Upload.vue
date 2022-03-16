0cd5d6befecaafea6509f5813044a3c846586ecb
Isilud <91018167+Isilud@users.noreply.github.com>
2022-03-16 12:18:00 +0100

    feat(webapp) box adding dropped file to upload

--- src/webapp/src/views/Upload.vue
+++ src/webapp/src/views/Upload.vue
@@ -1,22 +1,41 @@
 <template>
-  <h2>UPLOAD</h2>
-  <div v-if="this.status === 'None'">
-    <label>
-      Title
-      <input type="text" v-model="title" /> </label
-    ><br />
-    <label
-      >File
+  <h2 v-on:drop.prevent.stop>UPLOAD</h2>
+  <form class="form_upload" v-if="this.status === 'None'">
+    <div id="video_box">
       <input
+        id="file"
         type="file"
         ref="file"
-        v-on:change="handleFileUpload()"
+        @change="handleFileUpload()"
         accept="video/*"
+        required
       />
-    </label>
-    <br />
-    <button v-on:click="submitFile()">Submit</button>
-  </div>
+      <!-- Div handling drag-events when supported -->
+      <div
+        id="drag_box"
+        v-if="isAdvancedUpload()"
+        @drop.prevent.stop="handleDrop"
+        @dragleave.prevent.stop="dragOnBox(false)"
+        @dragend.prevent.stop="dragOnBox(false)"
+        @dragenter.prevent.stop="dragOnBox(true)"
+        @dragover.prevent.stop="dragOnBox(true)"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+      ></div>
+      <label id="box_text" for="file">
+        <strong>Choose a file</strong>
+        <span v-if="isAdvancedUpload()"
+          ><br />
+          or drag it here</span
+        >.
+      </label>
+    </div>
+    <div>
+      <span class="label">Title : </span>
+      <input class="input_title" type="text" v-model="title" required />
+    </div>
+    <button @click="submitFile()">Submit</button>
+  </form>
   <div v-else>
     <h5>{{ this.status }}</h5>
     <span>Uploading and transforming a video can be a lengthy process</span>
@@ -69,14 +88,91 @@ export default {
     handleFileUpload() {
       // v-model doesn't support file form type
       this.file = this.$refs.file.files[0];
+      this.title = this.file.name;
     },
     retry: function () {
       this.status = "None";
       this.title = "";
       this.file = "";
     },
+    isAdvancedUpload: function () {
+      var div = document.createElement("div");
+      return (
+        ("draggable" in div || ("ondragstart" in div && "ondrop" in div)) &&
+        "FormData" in window &&
+        "FileReader" in window
+      );
+    },
+    handleDrop: function (e) {
+      this.file = e.dataTransfer.files[0];
+      this.title = this.file.name;
+      this.dragOnBox(false);
+    },
+    dragOnBox: function (bool) {
+      var back = document.getElementById("video_box");
+      var front = document.getElementById("drag_box");
+      if (bool) {
+        back.classList.add("is_dragged_over");
+        front.classList.add("is_dragged_over");
+      } else {
+        back.classList.remove("is_dragged_over");
+        front.classList.remove("is_dragged_over");
+      }
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.form_upload {
+  margin: auto;
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  align-items: center;
+}
+
+#video_box {
+  display: flex;
+  float: right;
+  height: 200px;
+  width: 200px;
+  border: 5px rgb(175, 175, 255) dashed;
+  align-items: center;
+  justify-content: center;
+  &.is_dragged_over {
+    border-style: solid;
+    background-color: rgb(218, 218, 255);
+  }
+}
+
+#drag_box {
+  height: inherit;
+  width: inherit;
+  position: absolute;
+  &.is_dragged_over {
+    z-index: 2;
+  }
+}
+
+.input_title {
+  width: 200px;
+}
+
+.label {
+  font-size: larger;
+  font-weight: bold;
+}
+
+#box_text {
+  z-index: 1;
+  strong:hover {
+    text-decoration: underline;
+  }
+}
+
+#file {
+  display: none;
+}
+</style>
