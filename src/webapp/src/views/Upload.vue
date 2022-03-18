50f7846b560afda67245434c3c40c972712bf540
Isilud <91018167+Isilud@users.noreply.github.com>
2022-03-18 10:28:18 +0100

    feat(webapp) improved upload presentation - cookie expire when session close

--- src/webapp/src/views/Upload.vue
+++ src/webapp/src/views/Upload.vue
@@ -1,7 +1,7 @@
 <template>
   <h2 v-on:drop.prevent.stop>UPLOAD</h2>
-  <form class="form_upload" v-if="this.status === 'None'">
-    <div id="video_box">
+  <form class="form_upload flex_center" v-if="this.status === 'None'">
+    <div id="video_box" :class="{ active: fileSelected }">
       <input
         id="file"
         type="file"
@@ -10,37 +10,53 @@
         accept="video/*"
         required
       />
-      <!-- Div handling drag-events when supported -->
-      <div
-        id="drag_box"
-        v-if="isAdvancedUpload()"
-        @drop.prevent.stop="handleDrop"
-        @dragleave.prevent.stop="dragOnBox(false)"
-        @dragend.prevent.stop="dragOnBox(false)"
-        @dragenter.prevent.stop="dragOnBox(true)"
-        @dragover.prevent.stop="dragOnBox(true)"
-        @drag.prevent.stop=""
-        @dragstart.prevent.stop=""
-      ></div>
-      <label id="box_text" for="file">
-        <strong>Choose a file</strong>
-        <span v-if="isAdvancedUpload()"
-          ><br />
-          or drag it here</span
-        >.
-      </label>
+      <div class="in_video_box flex_center" v-if="!fileSelected">
+        <!-- Div handling drag-events when supported -->
+        <div
+          id="drag_box"
+          v-if="dragEnabled"
+          @drop.prevent.stop="handleDrop"
+          @dragleave.prevent.stop="dragOnBox(false)"
+          @dragend.prevent.stop="dragOnBox(false)"
+          @dragenter.prevent.stop="dragOnBox(true)"
+          @dragover.prevent.stop="dragOnBox(true)"
+          @drag.prevent.stop=""
+          @dragstart.prevent.stop=""
+        ></div>
+        <label id="box_text" for="file">
+          <strong>Choose a file</strong>
+          <span v-if="isAdvancedUpload()"
+            ><br />
+            or drag it here</span
+          >.
+        </label>
+      </div>
+      <div class="in_video_box flex_center" v-else>
+        <img
+          id="video_preview"
+          src="../assets/uxwing-camera.png"
+          alt="Video Set"
+        />
+        <div class="title_box flex_center">
+          <input
+            type="text"
+            placeholder="Enter a Title"
+            v-model="title"
+            required
+          />
+        </div>
+      </div>
     </div>
-    <div>
-      <span class="label">Title : </span>
-      <input class="input_title" type="text" v-model="title" required />
-    </div>
-    <button @click="submitFile()">Submit</button>
+    <span v-if="fileSelected"
+      ><button @click.stop.prevent="submitFile()">Submit</button>
+      <button id="cancel" @click.stop.prevent="retry()">Cancel</button></span
+    >
   </form>
   <div v-else>
     <h5>{{ this.status }}</h5>
     <span>Uploading and transforming a video can be a lengthy process</span>
     <br />
-    <span v-on:click="retry()">Retry</span>
+    <span @click="retry()">Retry</span>
   </div>
 </template>
 
@@ -57,8 +73,20 @@ export default {
       file: "",
     };
   },
+  computed: {
+    fileSelected: function () {
+      return !this.file == "";
+    },
+    dragEnabled: function () {
+      return this.isAdvancedUpload() && !this.fileSelected;
+    },
+  },
   methods: {
     submitFile: function () {
+      if (this.title == "") {
+        alert("Please enter a title.");
+        return;
+      }
       // Simple way to keep the user aware about what is happening
       this.status = "Uploading";
 
@@ -88,7 +116,6 @@ export default {
     handleFileUpload() {
       // v-model doesn't support file form type
       this.file = this.$refs.file.files[0];
-      this.title = this.file.name;
     },
     retry: function () {
       this.status = "None";
@@ -105,7 +132,6 @@ export default {
     },
     handleDrop: function (e) {
       this.file = e.dataTransfer.files[0];
-      this.title = this.file.name;
       this.dragOnBox(false);
     },
     dragOnBox: function (bool) {
@@ -124,27 +150,39 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.flex_center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .form_upload {
   margin: auto;
   width: fit-content;
-  display: flex;
   flex-direction: column;
   row-gap: 10px;
-  align-items: center;
 }
 
 #video_box {
-  display: flex;
   float: right;
   height: 200px;
   width: 200px;
   border: 5px rgb(175, 175, 255) dashed;
-  align-items: center;
-  justify-content: center;
+  border-radius: 10px;
+  overflow: hidden;
   &.is_dragged_over {
     border-style: solid;
     background-color: rgb(218, 218, 255);
   }
+  &.active {
+    border: 2px rgb(175, 175, 255) solid;
+  }
+}
+
+.in_video_box {
+  height: inherit;
+  width: inherit;
+  flex-direction: column;
 }
 
 #drag_box {
@@ -154,10 +192,6 @@ export default {
   &.is_dragged_over {
     z-index: 2;
   }
-}
-
-.input_title {
-  width: 200px;
 }
 
 .label {
@@ -174,5 +208,40 @@ export default {
 
 #file {
   display: none;
+}
+
+#video_preview {
+  width: 60%;
+  height: 60%;
+  margin: 10% 20%;
+}
+
+#cancel {
+  margin-left: 20px;
+  background-color: red;
+}
+
+.title_box {
+  height: 20%;
+  width: inherit;
+  background-color: black;
+  position: bottom;
+}
+
+#show_title {
+  flex-grow: 1;
+  color: white;
+  font-size: large;
+  width: 80%;
+}
+
+input {
+  border: none;
+  height: 100%;
+  width: 80%;
+  background: inherit;
+  color: white;
+  font-size: large;
+  outline: none;
 }
 </style>
