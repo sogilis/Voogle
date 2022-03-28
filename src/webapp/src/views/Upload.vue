098afcbca5365ad2e2271c6e033b2c25b10dd18a
Isilud <91018167+Isilud@users.noreply.github.com>
2022-03-28 17:00:22 +0200

    feat(webapp) added animation to link and button on the home page. Added effect when hovering over videos.

--- src/webapp/src/views/Upload.vue
+++ src/webapp/src/views/Upload.vue
@@ -1,6 +1,10 @@
 <template>
   <h2 v-on:drop.prevent.stop>UPLOAD</h2>
-  <form class="form_upload flex_center" v-if="this.status === 'None'">
+  <form
+    class="form_upload flex_center"
+    @submit.prevent="submitFile()"
+    v-if="this.status === 'None'"
+  >
     <div id="video_box" :class="{ active: fileSelected }">
       <input
         id="file"
@@ -52,11 +56,7 @@
       </div>
     </div>
     <span class="flex_center">
-      <button
-        class="button is-primary"
-        :disabled="!fileSelected"
-        @click.stop.prevent="submitFile()"
-      >
+      <button type="submit" class="button is-primary" :disabled="!fileSelected">
         <span>Upload</span>
         <span><i class="fa-solid fa-upload"></i></span>
       </button>
@@ -182,6 +182,7 @@ export default {
   border: 5px rgb(175, 175, 255) dashed;
   border-radius: 10px;
   overflow: hidden;
+  transition: background-color 400ms;
   &.is_dragged_over {
     border-style: solid;
     background-color: rgb(218, 218, 255);
@@ -260,7 +261,7 @@ input {
 button {
   width: 90px;
   margin: 0 10px;
-  & i {
+  i {
     margin-left: 5px;
   }
 }
