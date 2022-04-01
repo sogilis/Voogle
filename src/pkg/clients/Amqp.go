044f6deeb988893671e95679d0766dbb43b3c404
semanazc <clery.sem@gmail.com>
2022-04-01 15:47:46 +0200

    Add method queueDeclare to Amqp.go

--- src/pkg/clients/Amqp.go
+++ src/pkg/clients/Amqp.go
@@ -2,33 +2,30 @@ package clients
 
 import (
 	"github.com/streadway/amqp"
-
-	"github.com/Sogilis/Voogle/src/pkg/events"
 )
 
 type IAmqpClient interface {
 	Publish(nameQueue string, message []byte) error
 	Consume(nameQueue string) (<-chan amqp.Delivery, error)
+	QueueDeclare() (amqp.Queue, error)
 }
 
 var _ IAmqpClient = &amqpClient{}
 
 type amqpClient struct {
-	channel  *amqp.Channel
-	fullAddr string
+	channel   *amqp.Channel
+	fullAddr  string
+	queueName string
 }
 
 func NewAmqpClient(addr, user, pwd, queueName string) (IAmqpClient, error) {
 	amqpC := &amqpClient{
-		channel:  nil,
-		fullAddr: "amqp://" + user + ":" + pwd + "@" + addr + "/",
+		channel:   nil,
+		fullAddr:  "amqp://" + user + ":" + pwd + "@" + addr + "/",
+		queueName: queueName,
 	}
 
 	if err := amqpC.connect(); err != nil {
-		return nil, err
-	}
-
-	if _, err := amqpC.queueDeclare(queueName); err != nil {
 		return nil, err
 	}
 
@@ -47,14 +44,14 @@ func (r *amqpClient) Publish(nameQueue string, message []byte) error {
 		},
 	)
 
-	// If we cannot publish, try to reconnect to rabbitMQ service ONE time before
-	// return error
 	if err != nil {
 		if err = r.connect(); err != nil {
 			return err
 		}
 
-		if _, err := r.queueDeclare(events.VideoUploaded); err != nil {
+		// If we cannot publish, try to reconnect to rabbitMQ service ONE time before
+		// return error
+		if _, err := r.QueueDeclare(); err != nil {
 			return err
 		}
 
@@ -69,7 +66,6 @@ func (r *amqpClient) Publish(nameQueue string, message []byte) error {
 			},
 		)
 	}
-
 	return nil
 }
 
@@ -100,6 +96,6 @@ func (r *amqpClient) connect() error {
 	return nil
 }
 
-func (r *amqpClient) queueDeclare(name string) (amqp.Queue, error) {
-	return r.channel.QueueDeclare(name, false, false, false, false, nil)
+func (r *amqpClient) QueueDeclare() (amqp.Queue, error) {
+	return r.channel.QueueDeclare(r.queueName, false, false, false, false, nil)
 }
