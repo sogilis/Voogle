03925754d19fe9a6021bf43ad5e9ba95a62c557b
semanazc <clery.sem@gmail.com>
2022-04-01 14:21:03 +0200

    apply comments

--- docker/.mariadb/.schema/schema.sql
+++ docker/.mariadb/.schema/schema.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS videos (
-    id          VARCHAR(36) NOT NULL,
-    title       VARCHAR(64) NOT NULL,
-    v_status    INT NOT NULL, -- v_state, because state and status are SQL keywords
-    uploaded_at DATETIME,
-    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id              VARCHAR(36) NOT NULL,
+    title           VARCHAR(64) NOT NULL,
+    video_status    INT NOT NULL,
+    uploaded_at     DATETIME,
+    created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT pk PRIMARY KEY (id),
     CONSTRAINT unique_title UNIQUE (title)
 );
 
 CREATE TABLE IF NOT EXISTS uploads (
-    id          VARCHAR(36) NOT NULL,
-    v_id        VARCHAR(64) NOT NULL,
-    v_status    INT NOT NULL, -- v_state, because state and status are SQL keywords
-    uploaded_at DATETIME,
-    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id              VARCHAR(36) NOT NULL,
+    video_id        VARCHAR(36) NOT NULL,
+    upload_status   INT NOT NULL,
+    uploaded_at     DATETIME,
+    created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT pk PRIMARY KEY (id),
-    CONSTRAINT fk_v_id FOREIGN KEY (v_id) REFERENCES videos (id)
+    CONSTRAINT fk_v_id FOREIGN KEY (video_id) REFERENCES videos (id)
 );
