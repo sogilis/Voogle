bd2c6c8f495e0444d97c0ea3203cf650fd24c61e
semanazc <clery.sem@gmail.com>
2022-05-02 17:53:54 +0200

    Add more E2E tests

--- end2end/videos_test.go
+++ end2end/videos_test.go
@@ -71,6 +71,7 @@ func Test_Videos(t *testing.T) {
 
 					f, err := os.Open("../samples/1280x720_2mb.mp4")
 					assert.NoError(t, err)
+					defer f.Close()
 
 					// Post video upload
 					code, body, err := session.PostMultipart("/api/v1/videos/upload", "test data", "video.avi", f)
@@ -117,6 +118,60 @@ func Test_Videos(t *testing.T) {
 					assert.Equal(t, 1, videoData.LastPage)
 					assert.Equal(t, 1, len(videoData.Videos))
 					assert.Equal(t, "test data", videoData.Videos[0].Title)
+				})
+
+				g.It("Returns an error title already exist", func() {
+					t.Log("PATH - GET - " + path)
+
+					f, err := os.Open("../samples/1280x720_2mb.mp4")
+					assert.NoError(t, err)
+					defer f.Close()
+
+					// Post video upload
+					code, _, err := session.PostMultipart("/api/v1/videos/upload", "test data", "video.avi", f)
+					assert.NoError(t, err)
+
+					assert.Equal(t, 409, code)
+				})
+
+				g.It("Returns video stream master and first part", func() {
+					// Get video list
+					code, body, err := session.Get(path)
+					assert.NoError(t, err)
+
+					assert.Equal(t, 200, code)
+
+					// Reading the body
+					rawBody, err := ioutil.ReadAll(body)
+					g.Assert(err).IsNil()
+					var videoData helpers.AllVideos
+					err = json.Unmarshal(rawBody, &videoData)
+					assert.NoError(t, err)
+
+					assert.Equal(t, "Success", videoData.Status)
+					assert.Equal(t, 1, len(videoData.Data))
+					assert.Equal(t, "test data", videoData.Data[0].Title)
+
+					// Get video master
+					code, body, err = session.Get("/api/v1/videos/" + videoData.Data[0].Id + "/streams/master.m3u8")
+					assert.NoError(t, err)
+
+					assert.Equal(t, 200, code)
+
+					// Reading the body
+					rawBody, err = ioutil.ReadAll(body)
+					g.Assert(err).IsNil()
+
+					//Parse response
+					parseBody := strings.Split(string(rawBody), "#")
+					assert.Equal(t, "EXTM3U\n", parseBody[1])
+					assert.Equal(t, "EXT-X-VERSION:3\n", parseBody[2])
+
+					// Get video part
+					code, _, err = session.Get("/api/v1/videos/" + videoData.Data[0].Id + "/streams/v0/segment0.ts")
+					assert.NoError(t, err)
+
+					assert.Equal(t, 200, code)
 				})
 			})
 		})
