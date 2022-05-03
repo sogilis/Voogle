7707af461e2a3d2ba64c525a99373cb1311bf112
Théophile <theophile@sogilis.com>
2022-05-03 11:19:55 +0200

    fix(infra): remove resource organization sogilis

--- infra/voogle-sogilis-dev/organization.tf
+++
@@ -1,6 +0,0 @@
-variable "root_organization" {}
-
-resource "aws_organizations_organizational_unit" "voogle-sogilis-dev" {
-  name      = "voogle-sogilis-dev"
-  parent_id = var.root_organization
-}
