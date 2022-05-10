ab6032d65130491bb234e48d6c60489a32917f09
semanazc <clery@sogilis.com>
2022-05-10 16:37:24 +0200

    Fix one missing Failure case (doc) in status handler

--- src/cmd/api/docs/docs.go
+++ src/cmd/api/docs/docs.go
@@ -189,6 +189,12 @@ const docTemplate = `{
                             "type": "object"
                         }
                     },
+                    "404": {
+                        "description": "Not Found",
+                        "schema": {
+                            "type": "object"
+                        }
+                    },
                     "500": {
                         "description": "Internal Server Error",
                         "schema": {
