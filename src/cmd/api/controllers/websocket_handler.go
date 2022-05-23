7a7c67de59463b0a97a2768b86b2280bb03eb540
Isilud <jolan.houot@gmail.com>
2022-05-23 14:32:44 +0200

    Added queue to ws_handler

--- src/cmd/api/controllers/websocket_handler.go
+++ src/cmd/api/controllers/websocket_handler.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/Sogilis/Voogle/src/pkg/clients"
 
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 )
 
 type WSHandler struct {
+	AmqpExchangerStatus clients.IAmqpExchanger
 }
 
 func (wsh WSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -25,5 +29,34 @@ func (wsh WSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Error("Cannot send message : ", err)
 		return
+	}
+
+	q, err := wsh.AmqpExchangerStatus.QueueDeclare()
+	if err != nil {
+		log.Error("Could not create queue : ", err)
+		return
+	}
+
+	msgs, err := wsh.AmqpExchangerStatus.Consume(q)
+	if err != nil {
+		log.Error("Failed to register a consumer : ", err)
+		return
+	}
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	for {
+		// Read message from browser
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		// Print the message to the console
+		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
 	}
 }
