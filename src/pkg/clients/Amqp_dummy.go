cd1732291b12d5898c1bf5255898e16c704691ea
Isilud <jolan.houot@gmail.com>
2022-05-24 16:07:02 +0200

    Correct tests

--- src/pkg/clients/Amqp_dummy.go
+++ src/pkg/clients/Amqp_dummy.go
@@ -38,3 +38,48 @@ func (r amqpClientDummy) QueueDeclare() (amqp.Queue, error) {
 
 	return queue, nil
 }
+
+//-------------------------------------------------
+
+var _ IAmqpExchanger = amqpExchangerDummy{}
+
+type amqpExchangerDummy struct {
+	publish      func(string, []byte) error
+	consume      func(amqp.Queue) (<-chan amqp.Delivery, error)
+	queueDeclare func() (amqp.Queue, error)
+	queueBind    func(amqp.Queue, string) error
+}
+
+func NewAmqpExchangeDummy(publish func(string, []byte) error, consume func(amqp.Queue) (<-chan amqp.Delivery, error), queueDeclare func() (amqp.Queue, error), queueBind func(amqp.Queue, string) error) IAmqpExchanger {
+	return amqpExchangerDummy{publish, consume, queueDeclare, queueBind}
+}
+
+func (r amqpExchangerDummy) Publish(nameQueue string, message []byte) error {
+	if r.publish != nil {
+		return r.publish(nameQueue, message)
+	}
+	return nil
+}
+
+func (r amqpExchangerDummy) Consume(q amqp.Queue) (<-chan amqp.Delivery, error) {
+	if r.consume != nil {
+		return r.consume(q)
+	}
+	return nil, nil //nolint:nilnil
+}
+
+func (r amqpExchangerDummy) QueueDeclare() (amqp.Queue, error) {
+	if r.queueDeclare != nil {
+		return r.queueDeclare()
+	}
+	queue := amqp.Queue{}
+
+	return queue, nil
+}
+
+func (r amqpExchangerDummy) QueueBind(q amqp.Queue, key string) error {
+	if r.queueBind != nil {
+		return r.queueBind(q, key)
+	}
+	return nil
+}
