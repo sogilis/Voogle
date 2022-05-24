73fcc6a584a4168c5aa8790a6d1ac5f785652ebb
Isilud <jolan.houot@gmail.com>
2022-05-24 13:33:04 +0200

    add message handling to websocket_handler

--- src/cmd/api/controllers/websocket_handler.go
+++ src/cmd/api/controllers/websocket_handler.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"bytes"
 	"encoding/base64"
-	"fmt"
 	"net/http"
 
 	"github.com/streadway/amqp"
@@ -50,18 +49,18 @@ func (wsh WSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	err = conn.WriteMessage(websocket.TextMessage, []byte("Connection is a success."))
+	err = conn.WriteMessage(websocket.TextMessage, []byte("Connexion is a success."))
 	if err != nil {
 		log.Error("Cannot send message : ", err)
 		return
 	}
 
-	msgs, err := GetConsumer(&wsh)
+	msgs, q, err := GetConsumer(&wsh)
 	if err != nil {
 		log.Error("Could not create Consumer : ", err)
 	}
 
-	ReadMessage(msgs, conn)
+	HandleMessage(&wsh, *q, msgs, conn)
 }
 
 func decodeAuthorization(r *http.Request) (decodedData []byte, err error) {
@@ -87,38 +86,46 @@ func extractCredentials(data []byte) (username string, password string) {
 	return givenUser, givenPass
 }
 
-var GetConsumer = func(wsh *WSHandler) (<-chan amqp.Delivery, error) {
+var GetConsumer = func(wsh *WSHandler) (<-chan amqp.Delivery, *amqp.Queue, error) {
 
 	q, err := wsh.AmqpExchangerStatus.QueueDeclare()
 	if err != nil {
 		log.Error("Could not create queue : ", err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	msgs, err := wsh.AmqpExchangerStatus.Consume(q)
 	if err != nil {
 		log.Error("Failed to register a consumer : ", err)
-		return nil, err
+		return nil, nil, err
 	}
-	return msgs, nil
+	return msgs, &q, nil
 }
 
-var ReadMessage = func(msgs <-chan amqp.Delivery, conn *websocket.Conn) {
+var HandleMessage = func(wsh *WSHandler, q amqp.Queue, msgs <-chan amqp.Delivery, conn *websocket.Conn) {
+	// Read message from client
 	go func() {
-		for d := range msgs {
-			log.Printf(" [x] %s", d.Body)
+		for {
+			// Read message from browser
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			err = wsh.AmqpExchangerStatus.QueueBind(q, string(msg))
+			if err != nil {
+				log.Error("Could not bind queue : ", err)
+			}
 		}
 	}()
 
+	// Transfer message from queue to client
 	for {
-		// Read message from browser
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Error("Could not read message : ", err)
-			return
+		for d := range msgs {
+			err := conn.WriteMessage(websocket.TextMessage, []byte(d.Body))
+			if err != nil {
+				log.Error("Cannot send message : ", err)
+				return
+			}
 		}
-
-		// Print the message to the console
-		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
 	}
 }
