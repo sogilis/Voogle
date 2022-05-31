379065bcef2dd09feddd8adc8708fac30d988e81
Isilud <jolan.houot@gmail.com>
2022-05-31 10:39:48 +0200

    Created 'grayserver' and his transformation, added it to the services.

---
+++ src/cmd/grayserver/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	Port    uint32 `env:"PORT" envDefault:"50051"`
+	DevMode bool   `env:"DEV_MODE" envDefault:"false"`
+
+	S3Host    string `env:"S3_HOST" envDefault:""`
+	S3AuthKey string `env:"S3_AUTH_KEY,required"`
+	S3AuthPwd string `env:"S3_AUTH_PWD,required"`
+	S3Bucket  string `env:"S3_BUCKET" envDefault:"voogle-video"`
+	S3Region  string `env:"S3_REGION" envDefault:"eu-west-3"`
+}
+
+func NewConfig() (Config, error) {
+	config := Config{}
+
+	err := env.Parse(&config)
+
+	return config, err
+}
