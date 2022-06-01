1653706123bde8d1eef1bd7c5d95a17ec8c131ae
Isilud <91018167+Isilud@users.noreply.github.com>
2022-06-01 11:13:42 +0200

    feat (API) add ws endpoint (#352)
    * Added subrouter, endpoint
    
    * Added WSHandler
    
    * Added Websocket to front
    
    * add fixme for WS security
    
    Co-authored-by: Willy Malvault <willy@sogilis.com>

---
+++ src/cmd/api/controllers/websocket_handler.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+type WSHandler struct {
+}
+
+func (wsh WSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	upgrader := websocket.Upgrader{}
+	//FIXME: validate that disabling heckoriing for WS is OK
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Error("Cannot upgrade : ", err)
+		return
+	}
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte("Connection is a success."))
+	if err != nil {
+		log.Error("Cannot send message : ", err)
+		return
+	}
+}
