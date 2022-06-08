fdfc3bc033234642c5d4e06d61fd7bc7e0df6f58
Isilud <jolan.houot@gmail.com>
2022-06-08 13:46:11 +0200

    Clear video cache when changing filter

--- src/webapp/src/components/VideoPlayer.vue
+++ src/webapp/src/components/VideoPlayer.vue
@@ -1,10 +1,6 @@
 <template>
-  <div>
-    <video
-      class="video-js vjs-theme-forest"
-      :data-id="videoId"
-      controls
-    ></video>
+  <div class="video__container">
+    <video class="video-js vjs-theme-forest" ref="videoId" controls></video>
   </div>
 </template>
 
@@ -12,7 +8,7 @@
 import "video.js/dist/video-js.css";
 import "@videojs/themes/dist/forest/index.css";
 import videojs from "video.js";
-import "videojs-hls-quality-selector";
+import videojsqualityselector from "videojs-hls-quality-selector";
 import "videojs-contrib-quality-levels";
 import cookies from "js-cookie";
 
@@ -21,26 +17,50 @@ export default {
     videoId: String,
     filterlist: String,
   },
-  mounted() {
-    videojs.Vhs.xhr.beforeRequest = (options) => {
-      options.headers = options.headers || {};
-      options.headers.Authorization = cookies.get("Authorization");
-      options.uri += this.filterlist;
-      return options;
+  data() {
+    return {
+      videoPlayer: null,
+      timestamp: 0,
     };
-    const player = videojs(
-      document.querySelector("video[data-id='" + this.videoId + "']")
-    );
-    player.src(
-      process.env.VUE_APP_API_ADDR +
-        "api/v1/videos/" +
-        this.videoId +
-        "/streams/master.m3u8"
-    );
-    player.hlsQualitySelector({
+  },
+  computed: {
+    filteruri: function () {
+      return this.filterlist;
+    },
+  },
+  mounted() {
+    this.initializePlayer();
+    this.videoPlayer.responsive(false);
+    this.videoPlayer.hlsQualitySelector = videojsqualityselector;
+    this.videoPlayer.hlsQualitySelector({
       displayCurrentQuality: true,
     });
-    this.videoplayer = player;
+  },
+  beforeUpdate() {
+    this.videoPlayer.pause();
+    this.timestamp = this.videoPlayer.currentTime();
+    this.videoPlayer.reset();
+    this.initializePlayer();
+    this.videoPlayer.currentTime(this.timestamp);
+    this.videoPlayer.play();
+  },
+  methods: {
+    initializePlayer: function () {
+      videojs.Hls.xhr.beforeRequest = (options) => {
+        options.headers = options.headers || {};
+        options.headers.Authorization = cookies.get("Authorization");
+        options.uri += this.filteruri;
+        return options;
+      };
+      var player = videojs(this.$refs.videoId);
+      player.src(
+        process.env.VUE_APP_API_ADDR +
+          "api/v1/videos/" +
+          this.videoId +
+          "/streams/master.m3u8"
+      );
+      this.videoPlayer = player;
+    },
   },
 };
 </script>
@@ -49,5 +69,7 @@ export default {
 .video-js {
   display: block;
   margin: 0 auto;
+  height: 480px;
+  width: 853px;
 }
 </style>
