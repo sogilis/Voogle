4d73940ccc2f7bf985b186297e3014199f0d7885
Isilud <jolan.houot@gmail.com>
2022-07-28 12:57:50 +0200

    Added response to handler

--- src/cmd/api/controllers/video_transformer_list_handler.go
+++ src/cmd/api/controllers/video_transformer_list_handler.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	log "github.com/sirupsen/logrus"
+
+	jsonDTO "github.com/Sogilis/Voogle/src/cmd/api/dto/json"
 	"github.com/Sogilis/Voogle/src/pkg/clients"
 )
 
@@ -10,9 +14,24 @@ type VideoTransformerListHandler struct {
 	ServiceDiscovery clients.ServiceDiscovery
 }
 
+type TransformerServiceListResponse struct {
+	Services []jsonDTO.TransformerServiceJson `json:"services"`
+}
+
 func (v VideoTransformerListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("ok"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+	log.Debug("GET VideoTransformerListHandler")
+	existingServices := v.ServiceDiscovery.GetExistingServices()
+	services := []jsonDTO.TransformerServiceJson{}
+	for _, service := range existingServices {
+		services = append(services, jsonDTO.TransformerServiceToTransformerServiceJson(service))
 	}
+	payload, err := json.Marshal(TransformerServiceListResponse{Services: services})
+	if err != nil {
+		log.Error("Unable to parse data struct in json ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(payload)
 }
