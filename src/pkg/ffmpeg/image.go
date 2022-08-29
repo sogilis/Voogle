326d6c3b27acc78137e426370170735218a9cec1
Isilud <jolan.houot@gmail.com>
2022-08-29 16:27:57 +0200

    encode cover image to right size

--- src/pkg/ffmpeg/image.go
+++ src/pkg/ffmpeg/image.go
@@ -7,7 +7,9 @@ import (
 )
 
 func ConvertImg(srcpath string, dstpath string) error {
-	_, err := exec.Command("ffmpeg", "-i", srcpath, dstpath).CombinedOutput()
+	_, err := exec.Command("ffmpeg", "-i", srcpath, "-vf",
+		"crop='if(gt(iw, ih), ih, iw)':'if(gt(iw, ih), ih, iw )':'if(gt(iw, ih), (iw-ih)/2, 0)':'if(gt(iw,ih), 0, (ih-iw)/2)', scale=250:250",
+		dstpath).CombinedOutput()
 	if err != nil {
 		return err
 	}
